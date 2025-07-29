c2199a655c70e45eb7950a1a8a90b89a843a1a82
Sowmiya <sowmiya.a2023it@sece.ac.in>
2025-07-29 23:17:31 +0530

    updated-backend

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,14 +2,16 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
-const { upload, updateUser } = require('../controllers/authController');// Register & Login
+const { upload, updateUser } = require('../controllers/authController');
+
+// Register & Login
 router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
 
 // Update Profile
-// router.put('/update', authMiddleware, authController.updateUser);
 // Use upload middleware for single file upload named 'profilePicture'
 router.put('/update', authMiddleware, upload.single('profilePicture'), updateUser);
+
 // Logout
 router.post('/logout', authMiddleware, authController.logoutUser);
 
